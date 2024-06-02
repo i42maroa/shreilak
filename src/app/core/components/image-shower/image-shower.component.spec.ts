2558ed83400a894bd5c1498c5e12324f92b5a15e
Antonio Marín <amrodriguezjarote@gmail.com>
2024-06-02 13:19:20 +0200

    loading image status

---
+++ src/app/core/components/image-shower/image-shower.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageShowerComponent } from './image-shower.component';
+
+describe('ImageShowerComponent', () => {
+  let component: ImageShowerComponent;
+  let fixture: ComponentFixture<ImageShowerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ImageShowerComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ImageShowerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/core/components/resource-page/resource-page.component.ts
+++ src/app/core/components/resource-page/resource-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ResourceInterface } from '../../../data/interface/resource.interface';
+import { ImageShowerComponent } from '../image-shower/image-shower.component';
+import { LetterSComponentSvg } from '../../svg/letter-s/letter-s.component';
 
 @Component({
   selector: 'app-resource-page',
   standalone: true,
-  imports: [],
+  imports: [ImageShowerComponent, LetterSComponentSvg],
   templateUrl: './resource-page.component.html',
   styleUrl: './resource-page.component.css'
 })
